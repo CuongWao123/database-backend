3faae551ce0f5e45d5e491434d7ec4df7a948bc3
NLDenn <vietvh2004@gmail.com>
2024-12-10 18:49:23 +0700

    bangluong

--- src/main/java/com/example/database_backend/controller/BangChamCongController.java
+++ src/main/java/com/example/database_backend/controller/BangChamCongController.java
@@ -1,15 +1,13 @@
 package com.example.database_backend.controller;
 
 import com.example.database_backend.repository.BangChamCongRepository;
+import com.example.database_backend.repository.BangLuongRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/bangchamcong")
 @RestController
@@ -17,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 public class BangChamCongController {
     private BangChamCongRepository bangChamCongRepository ;
-
+private BangLuongRepository bangLuongRepository;
     @GetMapping("/getAll")
     private ResponseEntity<?> getAllBCC(){
         return new ResponseEntity<>(bangChamCongRepository.findAll(), HttpStatus.OK);
+    }
+    @GetMapping("/allluong")
+    private ResponseEntity<?> getAllBL(){
+        return new ResponseEntity<>(bangLuongRepository.findAll(), HttpStatus.OK);
     }
 
 }
